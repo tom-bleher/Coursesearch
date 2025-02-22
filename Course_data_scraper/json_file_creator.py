d3c7d5f6da1421f2af2483fed68a30ac8034edd5
avsha172 <86556665+avsha172@users.noreply.github.com>
2025-02-22 12:08:47 +0200

    scarper saves even more data

--- Course_data_scraper/json_file_creator.py
+++ Course_data_scraper/json_file_creator.py
@@ -1,66 +1,78 @@
 import re
+from bs4 import BeautifulSoup
 
+from get_courses_data import get_courses_data
 from course_requirements import get_course_requirements
 
-
-
-def get_course_data(match):
-    course_number = match[1].strip()  # First td is the course name
-    course_name = match[2].strip()  # Second td is the number
-    # Remove any HTML entities like &nbsp; and span tags
-    course_number = re.sub(r'-&nbsp;|<.*?>', '', course_number).strip()[0:9]
-    course_name = re.sub(r'&nbsp;|<.*?>', '', course_name).strip()
-    # Write the cleaned course name and number to the file
-    course_number = re.sub('-', '', course_number)
-    return course_number, course_name
-
-
-
 requirements_handle = r"javascript:open_win\('Drishot_L\.aspx\?kurs=([\w\d]{8})"  # str appears only if there are requi
-courses_dir = "Math_courses"
-def create_courses_json(html_files, get_course_requi = False):
-    output_data = []
+
+
+# courses_dir = "Math_courses"
+
+def clean_str(st):  # cleans unwanted characters from txt
+    return st.replace("\xa0", " ")
+
+
+def create_courses_json(html_files, get_course_requi=False):
+    output_data = {}
     for file_name in html_files:
         extracted_data = {}
         with open(file_name, "r", encoding="utf-8") as file:
-            html_content = file.read()
-            matches = re.findall(requirements_handle, html_content)
+            # html_content = file.read()
+            soup = BeautifulSoup(file, "html.parser")
+            matches = re.findall(requirements_handle, str(soup))
             for match in matches:
-                extracted_data[match] = True
-        # Define the regular expression to find <tr> elements with class "listtdbld"
-        pattern = (r'<tr.*?class=["\'](listtdbld|listtd)["\'].*?>.*?(?:<td.*?>(.*?)</td>.*?<td colspan=["\'].*?["\']>('
-                   r'.*?)</td>|<td[^>]*colspan=["\']7["\'][^>]*>(.*?)</td>).*?</tr>')
-        # Find all matches
-        matches = re.findall(pattern, html_content, re.DOTALL)
-
-        # Open a file to write the results
-        last = "a"
-        curr_entry = False
-        for match in matches:
-            if match[0] == 'listtdbld':
-                course_number, course_name = get_course_data(match)
-
-                if last != course_number:
-                    if course_number in extracted_data and get_course_requi:
-                        url = f"https://www.ims.tau.ac.il/tal/kr/Drishot_L.aspx?kurs={course_number}&kv=01&sem=20242"
-                        pre_req, parallel_req = get_course_requirements(url,
-                                                                        delay=1)  # delay to not spam the university to much
-                    else:
-                        pre_req, parallel_req = [], []
-                    curr_entry = {
-                        "course_name": course_name,
-                        "course_number": course_number,
-                        "course_link": f"https://www.ims.tau.ac.il/Tal/Syllabus/Syllabus_L.aspx?course={course_number}01&year=2024",
-                        "parallel_req": parallel_req,
-                        "pre_req": pre_req,
-                    }
-            elif match[0] == 'listtd' and curr_entry:  # If it's listtd, and we have a pending listtdbld
-                course_of_study = match[2].strip()  # Extract the <td> with colspan="7" (course number)
-                faculty, degree = course_of_study.split('/')
-                curr_entry["faculty"] = faculty
-                curr_entry["degree"] = degree
-                output_data.append(curr_entry)
-                curr_entry = []
-
-            last = course_number
+                extracted_data[match] = True  # to know which courses might have דרישות קדם
+            courses_data = get_courses_data(soup)
+        dict_creator(courses_data, extracted_data, get_course_requi, output_data)
     return output_data
+
+
+def dict_creator(courses_data, extracted_data, get_course_requi, output_data):
+    for course_data in courses_data:
+        course_number, course_group, course_name = r1_extractor(course_data[0])
+        faculty, major = course_data[1][1].split('/')
+        course_keys = course_data[2]
+        course_items = course_data[3]
+        if course_number in output_data:
+            course = output_data[course_number]
+            group_data = fill_group_data(course_items, course_keys)
+            group_data["group"] = course_group
+            course["specific_data"].append(group_data)
+        else:
+
+            if course_number in extracted_data and get_course_requi:
+                url = f"https://www.ims.tau.ac.il/tal/kr/Drishot_L.aspx?kurs={course_number}&kv=01&sem=20242"
+                pre_req, parallel_req = get_course_requirements(url,
+                                                                delay=1)  # delay to not spam the university to much
+            else:
+                pre_req, parallel_req = [], []
+            specific_data = []
+            group_data = fill_group_data(course_items, course_keys)
+            group_data["group"] = course_group
+            specific_data.append(group_data)
+            curr_entry = {
+                "course_name": course_name,
+                "course_number": course_number,
+                "course_link": f"https://www.ims.tau.ac.il/Tal/Syllabus/Syllabus_L.aspx?course={course_number}"
+                               f"{course_group}&year=2024",
+                "parallel_req": parallel_req,
+                "pre_req": pre_req,
+                "specific_data": specific_data
+            }
+            output_data[course_number] = curr_entry
+
+
+def fill_group_data(course_items, course_keys):
+    group_data = {}
+    for i in range(len(course_keys)):
+        group_data[clean_str(course_keys[i])] = clean_str(course_items[i])
+    return group_data
+
+
+def r1_extractor(r1_data):
+    course_numbers = ''.join(c for c in r1_data[0] if c.isdigit())
+    course_number, course_group = course_numbers[0:8], course_numbers[8:10]
+    course_name = r1_data[1]
+    return course_number, course_group, course_name
+
--- Course_data_scraper/main.py
+++ Course_data_scraper/main.py
@@ -2,9 +2,9 @@ import json
 from pathlib import Path
 from json_file_creator import create_courses_json
 
-n = 2
+n = 3
 files_names = []
-courses_dir = 'Physics_courses'
+courses_dir = 'Math_courses'
 for i in range(n):
     files_name = f"{courses_dir}/pg{i}.html"
     my_file = Path(files_name)
@@ -12,6 +12,5 @@ for i in range(n):
         files_names.append(files_name)
 
 output_data = create_courses_json(files_names, get_course_requi=False)  # getting course requi spams university pages
-print(output_data)
 with open(f"{courses_dir}\courses.json", "w", encoding="utf-8") as output_file:
     json.dump(output_data, output_file, ensure_ascii=False, indent=4)
