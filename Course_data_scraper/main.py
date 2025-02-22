3672cbd0d5cebe5efc9d938c93fc3e61e50cbcc5
avsha172 <86556665+avsha172@users.noreply.github.com>
2025-02-22 21:57:22 +0200

    some fixes to web scraper

--- Course_data_scraper/main.py
+++ Course_data_scraper/main.py
@@ -4,13 +4,24 @@ from json_file_creator import create_courses_json
 
 n = 3
 files_names = []
-courses_dir = 'Math_courses'
+courses_dir = 'Physics_courses'
+get_course_requi = False
 for i in range(n):
     files_name = f"{courses_dir}/pg{i}.html"
     my_file = Path(files_name)
     if my_file.is_file():
         files_names.append(files_name)
 
-output_data = create_courses_json(files_names, get_course_requi=False)  # getting course requi spams university pages
-with open(f"{courses_dir}\courses.json", "w", encoding="utf-8") as output_file:
-    json.dump(output_data, output_file, ensure_ascii=False, indent=4)
+output_data = create_courses_json(files_names, get_course_requi=get_course_requi)  # getting course requi spams university pages
+if get_course_requi:
+    with open(f"{courses_dir}\courses.json", "w", encoding="utf-8") as output_file:
+        json.dump(output_data, output_file, ensure_ascii=False, indent=4)
+else: # to keep existing requirements
+    with open(f"{courses_dir}\courses.json", "r", encoding="utf-8") as output_file:
+        courses_dict = json.load(output_file)
+    for course_num in output_data:
+        for key in output_data[course_num]:
+            if key != 'pre_req' and key != 'parallel_req':
+                courses_dict[course_num][key] = output_data[course_num][key]
+    with open(f"{courses_dir}\courses.json", "w", encoding="utf-8") as output_file:
+        json.dump(courses_dict, output_file, ensure_ascii=False, indent=4)
